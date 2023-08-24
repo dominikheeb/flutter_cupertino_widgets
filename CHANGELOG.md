e1a294f6eb3d4391e5bf5960816b937cb59dbee9
Gabriel Duss <gabriel.duss@zuehlke.com>
2023-08-24 08:24:06 +0200

    todos added

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
 ## 0.0.1
 
-* TODO: Describe initial release.
+- TODO: Describe initial release.
+- TODO: Second sidebar
+- TODO: store state (group collapsed)
+- TODO: indicator for unread messages/files
--- lib/flutter_cupertino_widgets.dart
+++ lib/flutter_cupertino_widgets.dart
@@ -2,3 +2,5 @@ library flutter_cupertino_widgets;
 
 export 'widgets/cupertino_navigation_split_view.dart';
 export 'widgets/cupertino_navigation_split_view_header.dart';
+export 'widgets/cupertino_sidebar_item_group.dart';
+export 'widgets/cupertino_sidebar_item.dart';
