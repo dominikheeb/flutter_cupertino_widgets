dee1c50200e18dac21db9564826e3dd7b0550670
Gabriel Duss <gabriel.duss@zuehlke.com>
2023-08-28 08:53:42 +0200

    sidebar detail added

---
+++ lib/widgets/cupertino_sidebar_detail.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class SidebarDetail extends StatelessWidget {
+  const SidebarDetail({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // todo add header with top bar logic
+    return Text("data");
+  }
+}
